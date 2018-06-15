28f6809f344f08e9cfa2f1d122cc809acd616e7c
iagohfs <iago_henriquefs@hotmail.com>
2018-06-15 10:20:45 +0200

    Add project files.

---
+++ Lab_T2_iago/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab_T2_iago.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
